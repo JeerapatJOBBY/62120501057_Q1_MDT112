2bc2dd2d2337cbbf86f9b52f3bcb64dde95d5569
job.ljee555@gmail.com <job.ljee555@gmail.com>
2020-04-22 01:25:34 +0700

    SuccessQ1

--- MDTQ1/MDTQ1.ino
+++ MDTQ1/MDTQ1.ino
@@ -1,8 +1,10 @@
+int Speed = 1; int time = 0;
+
 void setup(){
     pinMode(2,INPUT_PULLUP);
     Serial.begin(9600);
     for(int light = 3 ; light <= 13 ;light++){
-        pinMode(light,OUTPUT);
+    pinMode(light,OUTPUT);
     }
     tone(8,200,500);
     delay(250);
@@ -11,46 +13,69 @@ void setup(){
 }
 
 void loop(){
-    for (int lightup =0; lightup <= 13; lightup++) {
-        if (lightup == 8)
+    for (int light =3; light <= 13; light++) {
+        if (light == 8)
         {
             digitalWrite(8, 0);
             continue;
         }
-    digitalWrite(lightup, 1);
-    delay(100);
-    digitalWrite(lightup, 0);
+        digitalWrite(light, 1);
+        delay(100/(Speed));
+        digitalWrite(light, 0);
 
-        if (digitalRead(2)==0)
-        {
-            tone(8, 200,500);
-        }
-        else {
+            int x = 0; 
+            while (digitalRead(2)==0) //กดปุ่มค้างยังไงค่าก็นับแค่ค่าเดียว
+            {
+            x++;
+            delay(50);
+                if (x <= 1)
+                {
+                time++; //จำนวนครั้งที่กด
+                    for (int i = 1;i <= 1 ;i++) 
+                    {
+                    Speed = Speed * 2;//ค่าเพิ่มความเร็ว
+                    }
+                tone(8,400,100);
+                Serial.println("Faster X 2 (Current Speed : X" + String(Speed) + ")");
+
+                }
+            }
         
-        }
     }
-    for (int lightdown = 13; lightdown >= 3; lightdown--) {
-    
-        if (lightdown == 8)
+
+
+
+    for (int light =13; light >= 3; light--) {
+        if (light == 8)
         {
             digitalWrite(8, 0);
             continue;
         }
-    digitalWrite(lightdown, 1);
-    delay(100);
-    digitalWrite(lightdown, 0);
+        digitalWrite(light, 1);
+        delay(100/(Speed));
+        digitalWrite(light, 0);
 
-        if (digitalRead(2)==0)
-        {
-            tone(8, 200,500);
-        }
-        else {
+            int x = 0; 
+            while (digitalRead(2)==0) //กดปุ่มค้างยังไงค่าก็นับแค่ค่าเดียว
+            {
+            x++;
+            delay(50);
+                if (x <= 1)
+                {
+                time++; //จำนวนครั้งที่กด
+                    for (int i = 1;i <= 1 ;i++) 
+                    {
+                    Speed = Speed * 2;//ค่าเพิ่มความเร็ว
+                    }
+                tone(8,400,100);
+                Serial.println("Faster X 2 (Current Speed : X" + String(Speed) + ")");
+
+                }
+            }
         
-        }
     }
 
-    
-    
+
 }
 
 
